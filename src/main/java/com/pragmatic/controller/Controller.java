694c332aba4649c9c88805be35ad12a40a65b1a3
Litetook <litetook@gmail.com>
2024-08-19 10:34:09 +0200

    sasha last comments to do

--- src/main/java/com/pragmatic/controller/Controller.java
+++ src/main/java/com/pragmatic/controller/Controller.java
@@ -15,10 +15,15 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 public class Controller {
 
-    @Autowired
+    final
     AccountService accountService;
-    @Autowired
-    private AccountRepository accountRepository;
+    private final AccountRepository accountRepository;
+//    сюди інжектити сервіси, не репозиторії
+//    в сервысы мын валыдацыю, а вже в ньому дьоргати репо
+    public Controller(AccountService accountService, AccountRepository accountRepository) {
+        this.accountService = accountService;
+        this.accountRepository = accountRepository;
+    }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "Teest") String name) {
@@ -40,8 +45,25 @@ public class Controller {
 
     }
 
+//   Те що зовні - не в базу, мають бути різні об'єкти перед передачою. Наприклад дто, наприклад акк дто ( дата транас обж)
+//    Не завжди треба віддаватти всю інфу як в базі
+//    один на рівні моделі ( те що в базу)
+//    дто це те що віддаю наверх
+//
     @PostMapping(path= "addAccount", consumes = "application/json", produces = "application/json" )
     public ResponseEntity<Account> addAccount(@RequestBody Account account) {
+
+//        тут віддати аккаунт дто
+//        окремо реквест, окремо резпонз
+//      request, response, і те що до бази.
+//        акк сервіс буде віддавати модель. її треба буде перегнати в дто
+//        створення - контроллер (валідація на рівні арі), сервіс (передаю сирі дані, валідація на те чи все ок з акком загалом, чи є, чи нема, пробувати кидати через ексепшени),
+//        далі репо. Почитати за екс зендлери, щоб створити для аккаунта свій
+//        гет - контроллер, сервіс, і репозиторій. Якщо по ід і помилки нема - то треба віддати помилку. Спробувати віддати ліст ( наприклад всіх користувачів)
+//        mvc описано спрінговий
+//        update можна спробувати
+
+
         Account newAccount = accountRepository.addAccount(account);
         return new ResponseEntity<>(newAccount, HttpStatus.CREATED);
     }
